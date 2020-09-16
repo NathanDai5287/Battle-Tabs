cd0ae6db3e1e2c5fa2eb651eeddc0b1e134e9dee
Nathan Dai <nathandai2000@gmail.com>
2020-09-15 17:35:12 -0700

    nearest ship

--- board.py
+++ board.py
@@ -1,5 +1,4 @@
 import random
-import numpy as np
 
 from ship import Ship
 
@@ -22,23 +21,7 @@ class Board:
 			for col in range(sidelength):
 				self.board[(row, col)] = False
 
-		for rock_coord in self.rocks:
-			self.board[rock_coord] = None
-
 		self.setup_board()
-
-	@staticmethod
-	def array_to_coord(array: np.ndarray) -> tuple:
-		"""converts an array to tuple form
-
-		Args:
-			array (np.ndarray): [[x], [y]]
-
-		Returns:
-			tuple: (x, y)
-		"""
-
-		return tuple(array.flatten())
 
 	def setup_board(self) -> None:
 		"""changes self.board values to represent ships
@@ -66,11 +49,40 @@ class Board:
 
 		initial = self.board.copy()
 		coords = ship.get_coords()
-		if (not(ship.in_boundary())):
+		if (not(ship.in_boundary())): # if the entire ship is on the board
 			return False
+
 		for coord in [tuple(arr) for arr in coords.transpose()]:
-			if (self.board[coord]):
+			if (self.board[coord]): # if the space is already taken
 				self.board = initial.copy()
 				return False
-			self.board[coord] = True
+			self.board[coord] = True # place ship
 		return True
+
+	def nearest_ship(self, coord: tuple) -> int:
+		"""finds the distance to the nearest ship on the board
+
+		Args:
+			coord (tuple): coordinate guessed
+
+		Returns:
+			int: distance
+		"""
+
+		if (self.board[coord]):
+			return 0 # if a ship was hit
+
+		hit_row, hit_col = coord
+		max_distance = max(abs(hit_row - self.sidelength), abs(hit_col - self.sidelength), hit_row, hit_col)
+		for distance in range(1, max_distance + 1):
+			for row in range(-distance, distance + 1):
+				col = distance - abs(row)
+
+				scan_row = hit_row + row
+				for i in [-1, 1]:
+					scan_col = hit_col + col * i
+					if (scan_row >= 0 and scan_row < self.sidelength and scan_col >= 0 and scan_col < self.sidelength): # check if in boundary
+						if (self.board[(scan_row, scan_col)]):
+							return abs(row) + abs(col)
+
+		return False # board is empty
